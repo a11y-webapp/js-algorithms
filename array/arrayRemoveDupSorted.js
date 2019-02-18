0d7254bac1508c7bc8bb67cc1b14540b1ab31573
a11y-webapp <Nika@Nikas-MacBook-Air.local>
2019-02-18 16:01:55 +0100

    new_problem: Remove duplicates from sorted array

---
+++ array/arrayRemoveDupSorted.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let j = 0;
+    
+    const length = nums.length;
+    
+    if (length < 1) {
+        return false;
+    }
+    
+    for (let i = 0; i < length; i++) {
+        if (nums[i] !== nums[i+1]) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    
+    return j;
+};
