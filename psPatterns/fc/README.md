4bfe2df4fde2ae43338baa398368cda4561cf120
a11y-webapp <Nika@Nikas-MacBook-Air.local>
2019-04-04 15:24:54 +0200

    psp: fc

---
+++ psPatterns/fc/README.md
@@ -0,0 +1,44 @@
+## Frequency Counter
+
+This pattern uses object or set to collect values and frequencies of values. It helps to avoing nested loops meaning avoid O(n^2) with array and string
+
+1. Problem: Given two arrays, write a function to check if second array contains squared values of first array.
+Input/Output:
+```javascript
+([1, 2, 3], [4, 9, 1]) => true;
+([1, 2, 3], [4, 1]) => false;
+([1, 2, 3], [1, 9, 1]) => false;
+```
+Solution:
+
+```javascript
+//Time: O(n) Space: O(n)
+var sameFC = function (arr1, arr2) {
+    let fcObj1 = {};
+    let fcObj2 = {};
+
+    //check if lengths are equal
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    //fill in objs with val of arr as a key and freq as a value
+    for (let val of arr1) {
+        fcObj1[val] = (fcObj1[val] || 0) + 1;
+    }
+
+    for (let val of arr2) {
+        fcObj2[val] = (fcObj2[val] || 0) + 1;
+    }
+
+    //check if squared val is in the second obj and their vals are ===
+    for (let val in fcObj1) {
+        if (!(val**2 in fcObj2) || fcObj1[val] !== fcObj2[val**2]) {
+            return false;
+        } 
+    }
+
+    return true;
+
+}
+```
---
+++ psPatterns/fc/fc.js
@@ -0,0 +1,81 @@
+// First naive approach
+// Time: O(n^2) Space: O(1) 
+var same = function (arr1, arr2) {
+    //check if lengths are equal
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    //take each val from the first arr1
+    //loop though the arr2
+    //if val*val is in the arr2
+    //replace the val*val from arr2 with some char to avoid deleting
+    for (let val of arr1) {
+        let valIndx = arr2.indexOf(val*val);
+        if (valIndx > -1) {
+            arr2[valIndx] = "a";
+        } else {
+            return false;
+        }
+    }
+
+    //use function every to check is all val in arr2 === char
+    return arr2.every((val) => val === "a");
+};
+
+// Solution using sums
+// Time: O(n) Space:O(1)
+var sameSum = function (arr1, arr2) {
+    let sum1 = 0;
+    let sum2 = 0;
+
+    //check if lengths are equal
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    //loop thought arr1 
+    //square all elements in arr1 and sum up them
+    for (let val of arr1) {
+        sum1 += val**2;
+    }
+
+    //loop arr2
+    //sum all elements in arr2
+    for (let val2 of arr2) {
+        sum2 += val2;
+    }
+
+    //compare results
+    return sum1 === sum2;
+};
+
+// Frequency Counter solution
+//Time: O(n) Space: O(n)
+var sameFC = function (arr1, arr2) {
+    let fcObj1 = {};
+    let fcObj2 = {};
+
+    //check if lengths are equal
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    //fill in objs with val of arr as a key and freq as a value
+    for (let val of arr1) {
+        fcObj1[val] = (fcObj1[val] || 0) + 1;
+    }
+
+    for (let val of arr2) {
+        fcObj2[val] = (fcObj2[val] || 0) + 1;
+    }
+
+    //check if squared val is in the second obj and their vals are ===
+    for (let val in fcObj1) {
+        if (!(val**2 in fcObj2) || fcObj1[val] !== fcObj2[val**2]) {
+            return false;
+        } 
+    }
+
+    return true;
+};
